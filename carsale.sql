db302f9eeb00ca969f1fa04eee0fee6d421646a6
zoro6789 <47236195+zoro6789@users.noreply.github.com>
2019-11-23 14:30:51 +0700

    upload database

---
+++ carsale.sql
@@ -0,0 +1,47 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.2.14-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping data for table carsale.car: ~0 rows (approximately)
+/*!40000 ALTER TABLE `car` DISABLE KEYS */;
+/*!40000 ALTER TABLE `car` ENABLE KEYS */;
+
+-- Dumping data for table carsale.city_fee: ~0 rows (approximately)
+/*!40000 ALTER TABLE `city_fee` DISABLE KEYS */;
+/*!40000 ALTER TABLE `city_fee` ENABLE KEYS */;
+
+-- Dumping data for table carsale.comment: ~0 rows (approximately)
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+
+-- Dumping data for table carsale.description: ~0 rows (approximately)
+/*!40000 ALTER TABLE `description` DISABLE KEYS */;
+/*!40000 ALTER TABLE `description` ENABLE KEYS */;
+
+-- Dumping data for table carsale.image: ~0 rows (approximately)
+/*!40000 ALTER TABLE `image` DISABLE KEYS */;
+/*!40000 ALTER TABLE `image` ENABLE KEYS */;
+
+-- Dumping data for table carsale.post: ~0 rows (approximately)
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+
+-- Dumping data for table carsale.spec: ~0 rows (approximately)
+/*!40000 ALTER TABLE `spec` DISABLE KEYS */;
+/*!40000 ALTER TABLE `spec` ENABLE KEYS */;
+
+-- Dumping data for table carsale.user: ~0 rows (approximately)
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
