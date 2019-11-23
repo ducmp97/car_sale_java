e36b5dabb2ce33c0ef59eb91f8cdb46a4c287817
zoro6789 <47236195+zoro6789@users.noreply.github.com>
2019-11-23 16:28:49 +0700

    Add files via upload

--- carsale.sql
+++ carsale.sql
@@ -1,17 +1,19 @@
 CREATE DATABASE carsale CHARACTER SET utf8 COLLATE utf8_general_ci;
 use carsale;
-CREATE TABLE description(
-	des_id INT AUTO_INCREMENT,
-	title VARCHAR(100) NOT NULL,
-	content VARCHAR(500) NOT NULL,
-	type_des VARCHAR(50) NOT NULL,
-	PRIMARY KEY(des_id)
-);
 CREATE TABLE image(
 	image_id INT AUTO_INCREMENT,
 	url VARCHAR(100) NOT NULL,
 	type_image VARCHAR(50) NOT NULL, -- loai image: post, car, spec
 	PRIMARY KEY(image_id)
+);
+CREATE TABLE description(
+	des_id INT AUTO_INCREMENT,
+	title VARCHAR(100) NOT NULL,
+	content VARCHAR(500) NOT NULL,
+	type_des VARCHAR(50) NOT NULL,
+	image_id INT NOT NULL,
+	PRIMARY KEY(des_id),
+	FOREIGN KEY(image_id) REFERENCES image(image_id)
 );
 CREATE TABLE car(
 	car_id INT AUTO_INCREMENT,
@@ -44,22 +46,22 @@ CREATE TABLE comment(
 	content VARCHAR(500) NOT NULL,
 	time_comment DATE NOT NULL,
 	user_id INT NOT NULL,
+	post_id INT NOT NULL,
 	PRIMARY KEY(comment_id),
-	FOREIGN KEY (user_id) REFERENCES user(user_id)
+	FOREIGN KEY (user_id) REFERENCES user(user_id),
+	FOREIGN KEY (post_id) REFERENCES post(post_id)
 );
 CREATE TABLE post(
 	post_id INT AUTO_INCREMENT,
 	title VARCHAR(100) NOT NULL,
 	content VARCHAR(500) NOT NULL,
 	time_update DATE NOT NULL,
-	comment_id INT ,
 	user_id INT NOT NULL,
 	TYPE VARCHAR(50) NOT NULL,
 	PRIMARY KEY (post_id),
-	FOREIGN KEY (user_id) REFERENCES user(user_id),
-	FOREIGN KEY (comment_id) REFERENCES comment(comment_id)
+	FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
-CREATE TABLE spec(
+CREATE TABLE spec( --thong so ky thuat
 	spec_id INT AUTO_INCREMENT,
 	title VARCHAR(100) NOT NULL,
 	spec_name VARCHAR(50) NOT NULL,
@@ -67,10 +69,8 @@ CREATE TABLE spec(
 	dram VARCHAR(20) NOT NULL,
 	detail VARCHAR(50) NOT NULL,
 	car_id INT NOT NULL,
-	image_id INT NOT NULL,
 	PRIMARY KEY (spec_id),
-	FOREIGN KEY(car_id) REFERENCES car(car_id),
-	FOREIGN KEY(image_id) REFERENCES image(image_id)
+	FOREIGN KEY(car_id) REFERENCES car(car_id)
 );
 CREATE TABLE city_fee(
 	city_fee_id INT AUTO_INCREMENT,
